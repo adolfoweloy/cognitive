89c221cf13648254bdaff90f1be45f4d264623e1
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2018-08-15 19:26:28 -0300

    add serverproject + rearrange the directory structure

---
+++ cognitive-server/src/main/java/com/adolfo/cognitive/CognitiveApplication.java
@@ -0,0 +1,12 @@
+package com.adolfo.cognitive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitiveApplication.class, args);
+	}
+}
---
+++ cognitive-server/src/test/java/com/adolfo/cognitive/CognitiveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adolfo.cognitive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CognitiveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
